b64e1c9f4b2f570e1bd8549265f05cdd41b6d678
norway240 <conman240@gmail.com>
2014-10-09 15:32:23 -0500

    Extract specific files fixed

--- src/com/norway240/mifu/Extract.java
+++ src/com/norway240/mifu/Extract.java
@@ -9,7 +9,7 @@ import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
-public class  Extract {
+public class  Extract { //http://filez.norway240.com/ksp/kspmods.txt
 	public static void ExtractZipFile(String path, String strZipFile, String destination, String specific){
 		try{
 	         File fSourceZip = new File(path+strZipFile);
@@ -25,34 +25,37 @@ public class  Extract {
 	        
 	         while(e.hasMoreElements()){
 	             ZipEntry entry = (ZipEntry)e.nextElement();
-	             int len = specific.length()-1;
-	             if(entry.getName().substring(0, len).equals(specific)){
-		             File destinationFilePath = new File(zipPath,entry.getName());
-		
-		             //create directories if required.
-		             destinationFilePath.getParentFile().mkdirs();
-		            
-		             //if the entry is directory, leave it. Otherwise extract it.
-		             if(entry.isDirectory()){
-		                     continue;
+	             int slen = specific.length();
+	             
+	             if(entry.getName().length()>=slen){
+		             if(entry.getName().substring(0, slen).equals(specific)){
+			             File destinationFilePath = new File(zipPath,entry.getName().substring(slen));
+			
+			             //create directories if required.
+			             destinationFilePath.getParentFile().mkdirs();
+			            
+			             //if the entry is directory, leave it. Otherwise extract it.
+			             if(entry.isDirectory()){
+			                     continue;
+			             }else{
+			                 System.out.println("Extracting " + destinationFilePath);
+			                 BufferedInputStream bis = new BufferedInputStream(zipFile.getInputStream(entry));                                                  
+			                 int b;
+			                 byte buffer[] = new byte[1024];
+			                 FileOutputStream fos = new FileOutputStream(destinationFilePath);
+			                 BufferedOutputStream bos = new BufferedOutputStream(fos, 1024);
+			
+			                 while ((b = bis.read(buffer, 0, 1024)) != -1) {
+			                                 bos.write(buffer, 0, b);
+			                 }
+			                 bos.flush();
+			                 bos.close();
+			                 bis.close();
+			             }
+	
 		             }else{
-		                 System.out.println("Extracting " + destinationFilePath);
-		                 BufferedInputStream bis = new BufferedInputStream(zipFile.getInputStream(entry));                                                  
-		                 int b;
-		                 byte buffer[] = new byte[1024];
-		                 FileOutputStream fos = new FileOutputStream(destinationFilePath);
-		                 BufferedOutputStream bos = new BufferedOutputStream(fos, 1024);
-		
-		                 while ((b = bis.read(buffer, 0, 1024)) != -1) {
-		                                 bos.write(buffer, 0, b);
-		                 }
-		                 bos.flush();
-		                 bos.close();
-		                 bis.close();
+		            	 System.out.println("Finding file...");
 		             }
-
-	             }else{
-	            	 System.out.println("Finding file...");
 	             }
 	         }
 	         MIFU.modsDisplay.append("Extracted: "+strZipFile+"\n");
